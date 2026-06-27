30e7a160134232f5d5a8253e441fb43db338458c
michiooo290-rgb <michiooo290@gmail.com>
2026-06-28 00:27:57 +0700

    fix scanner

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import FloatingHeader from "../../components/FloatingHeader";
-import Hero from "../../components/Hero";
-import URLScanner from "../../components/URLScanner";
-import Features from "../../components/Features";
-import PhishingDB from "../../components/PhishingDB";
-import Education from "../../components/Education";
-import HowItWorks from "../../components/HowItWorks";
-import CTA from "../../components/CTA";
-import Footer from "../../components/Footer";
+import FloatingHeader from "../components/FloatingHeader";
+import Hero from "../components/Hero";
+import URLScanner from "../components/URLScanner";
+import Features from "../components/Features";
+import PhishingDB from "../components/PhishingDB";
+import Education from "../components/Education";
+import HowItWorks from "../components/HowItWorks";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
+import StatsBar from "../components/StatsBar";
 
 export default function Home() {
   return (
