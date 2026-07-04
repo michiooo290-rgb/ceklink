b5dcd8e3eb0e1bc483e91c446e0c523d8a207204
michiooo290-rgb <michiooo290@gmail.com>
2026-07-04 13:11:18 +0700

    style: use cool MeshBackground variant (no amber orb)

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ import FAQ from "../components/FAQ";
 export default function Home() {
   return (
     <>
-      <MeshBackground intensity="medium" />
+      <MeshBackground variant="cool" intensity="medium" />
       <FloatingHeader />
       <main>
         <Hero />
