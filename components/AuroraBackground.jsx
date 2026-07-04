b5ce36db0681ef5706ea40d3f21ae88c77e6628a
michiooo290-rgb <michiooo290@gmail.com>
2026-07-04 16:25:36 +0700

    feat: add Aurora + starfield background (Deep Space)

---
+++ components/AuroraBackground.jsx
@@ -0,0 +1,8 @@
+export default function AuroraBackground() {
+  return (
+    <div className="aurora-bg" aria-hidden="true">
+      <div className="aurora-band" />
+      <div className="aurora-stars" />
+    </div>
+  );
+}
